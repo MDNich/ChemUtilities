89b8383fa310e0bc0444044837f7c10fb0b3f3a9
MDNich <71262643+MDNich@users.noreply.github.com>
2024-07-16 12:35:29 -0400

    Initial Commit

---
+++ ChemUtilities/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  ChemUtilities
+//
+//  Created by שמים זורעי כוכבים on 7/16/24.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
